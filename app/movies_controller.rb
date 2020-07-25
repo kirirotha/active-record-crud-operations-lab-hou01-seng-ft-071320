cbd2884aa4cdf971d3d1813fcfba80c7a1172913
Kiri Um <kirirotha@gmail.com>
2020-07-25 18:41:32 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -78,8 +78,8 @@ end
 def can_update_using_update_method
   # Update movie title to "Wat, huh?"
   Movie.create(title: "Wat?")
-  movie = movie.update(title: "Wat, huh?")
-  movie
+  Movie.update(title: "Wat, huh?")
+  
 end
 
 def can_update_multiple_items_at_once
